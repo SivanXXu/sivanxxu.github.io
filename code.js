005a7d82ce5456ce7f01e95ba888829252a3fb96
TokkiS <tokkitakitwo@gmail.com>
2023-07-29 18:21:46 -0400

    first stage

--- code.js
+++ code.js
@@ -22,3 +22,54 @@ document.addEventListener('DOMContentLoaded', function() {
     fadeElementInOnScroll();
     window.addEventListener('scroll', fadeElementInOnScroll);
 });
+
+const button = document.querySelector("#button");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['🍁', '🀄'],
+        confettiNumber: 10,
+    });
+})
+
+button2.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['💻', '⚡️'],
+        confettiNumber: 10,
+    });
+})
+
+button3.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['♟️', '🤖', '✖️', '➗', '🏹'],
+        confettiNumber: 15,
+    });
+})
+
+function setClasses(el) {
+    const isScrollable = el.scrollHeight > el.clientHeight;
+    
+    // GUARD: If element is not scrollable, remove all classes
+    if (!isScrollable) {
+      el.classList.remove('is-bottom-overflowing', 'is-top-overflowing');
+      return;
+    }
+    
+    // Otherwise, the element is overflowing!
+    // Now we just need to find out which direction it is overflowing to (can be both)
+    const isScrolledToBottom = el.scrollHeight <= el.clientHeight + el.scrollTop;
+    const isScrolledToTop = isScrolledToBottom ? false : el.scrollTop === 0;
+    el.classList.toggle('is-bottom-overflowing', !isScrolledToBottom);
+    el.classList.toggle('is-top-overflowing', !isScrolledToTop);
+  }
+  
+  document.querySelector('#content').addEventListener('scroll', (e) => {
+    const el = e.currentTarget;
+    setClasses(el);
+  });
+  
+  setClasses(document.querySelector('#content'));
